290428a53e6dd8dc4d0764ea9a2fbb0898e8d97a
kushmirc <107213928+kushmirc@users.noreply.github.com>
2022-09-24 12:15:32 -0600

    Add model package with classes: Inventory, Part, Product, InHouse, Outsourced

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module mirchandani.inventorysystem {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens mirchandani.inventorysystem to javafx.fxml;
     exports mirchandani.inventorysystem;
 }

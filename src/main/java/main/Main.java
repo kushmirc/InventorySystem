947ad278a09e7f4a0d58836ec2a72e8bf267d088
kushmirc <107213928+kushmirc@users.noreply.github.com>
2022-09-25 17:29:05 -0600

    Update layouts for AddPart.fxml

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("MainScreen.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 1200, 400);
         stage.setTitle("Inventory Management System");
         stage.setScene(scene);
         stage.show();

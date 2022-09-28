9568b03f8eed1c040db4194b554b8b09d2c1cd63
kushmirc <107213928+kushmirc@users.noreply.github.com>
2022-09-28 17:23:58 -0600

    Add getters and setters to InHouse and Outsourced classes.

--- src/main/java/model/OutSourced.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class OutSourced extends Part{
-
-    private String companyName;
-    public OutSourced(int id, String name, double price, int stock, int min, int max, String companyName) {
-        super(id, name, price, stock, min, max);
-    }
-}

ac8dc1ea08bd34add243e01612cc147c6c53ae1f
kushmirc <107213928+kushmirc@users.noreply.github.com>
2022-09-28 17:26:10 -0600

    Updated constructors in InHouse and Outsourced classes.

--- src/main/java/model/Outsourced.java
+++ src/main/java/model/Outsourced.java
@@ -5,6 +5,7 @@ public class Outsourced extends Part{
     private String companyName;
     public Outsourced(int id, String name, double price, int stock, int min, int max, String companyName) {
         super(id, name, price, stock, min, max);
+        this.machineId = machineId;
     }
 
     public String getCompanyName() {

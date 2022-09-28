81567d34464c29ee60693d73e9fc371519b279d7
kushmirc <107213928+kushmirc@users.noreply.github.com>
2022-09-28 17:19:52 -0600

    Update constructor methods in InHouse and Outsourced classes.

--- src/main/java/model/InHouse.java
+++ src/main/java/model/InHouse.java
@@ -3,7 +3,7 @@ package model;
 public class InHouse extends Part {
 
     private int machineId;
-    public InHouse(int id, String name, double price, int stock, int min, int max) {
+    public InHouse(int id, String name, double price, int stock, int min, int max, int machineId) {
         super(id, name, price, stock, min, max);
 
 
--- src/main/java/model/OutSourced.java
+++ src/main/java/model/OutSourced.java
@@ -3,7 +3,7 @@ package model;
 public class OutSourced extends Part{
 
     private String companyName;
-    public OutSourced(int id, String name, double price, int stock, int min, int max) {
+    public OutSourced(int id, String name, double price, int stock, int min, int max, String companyName) {
         super(id, name, price, stock, min, max);
     }
 }

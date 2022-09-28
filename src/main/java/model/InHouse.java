b8dc6e324a487126b30f34d6e51d1df5c2b63834
kushmirc <107213928+kushmirc@users.noreply.github.com>
2022-09-28 17:17:51 -0600

    Add constructor methods and class variables to InHouse and Outsourced classes.

--- src/main/java/model/InHouse.java
+++ src/main/java/model/InHouse.java
@@ -1,4 +1,11 @@
 package model;
 
-public class InHouse {
+public class InHouse extends Part {
+
+    private int machineId;
+    public InHouse(int id, String name, double price, int stock, int min, int max) {
+        super(id, name, price, stock, min, max);
+
+
+    }
 }
--- src/main/java/model/OutSourced.java
+++ src/main/java/model/OutSourced.java
@@ -1,4 +1,9 @@
 package model;
 
-public class OutSourced {
+public class OutSourced extends Part{
+
+    private String companyName;
+    public OutSourced(int id, String name, double price, int stock, int min, int max) {
+        super(id, name, price, stock, min, max);
+    }
 }

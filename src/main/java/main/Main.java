6266b4a18282f47debafce4c7c15f0ef091c6989
kushmirc <107213928+kushmirc@users.noreply.github.com>
2022-09-29 11:10:07 -0600

    Add test data, 3 Part objects and 3 Product objects by calling constructors in Main.java.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,6 +4,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import model.InHouse;
+import model.Inventory;
+import model.Outsourced;
+import model.Product;
 
 import java.io.IOException;
 
@@ -18,6 +22,15 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
+
+        InHouse part1 = new InHouse(1, "flipper", 8.00, 23, 5, 50, 1);
+        InHouse part2 = new InHouse(2, "bumper", 15.00, 16, 3, 25, 2);
+        Outsourced part3 = new Outsourced(3, "ramp", 35.00, 8, 2, 15, RampsRUs);
+
+        Product product1 = new Product(1000, "The Addams Family", 3000, 6, 2, 10);
+        Product product2 = new Product(1001, "Terminator 2", 2000, 3, 1, 5);
+        Product product3 = new Product(1002, "Who Dunnit", 2500, 4, 3, 12);
+
         launch();
     }
 }

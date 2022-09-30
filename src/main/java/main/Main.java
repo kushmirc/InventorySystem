b1a2eb3a72412e3f404dc253a67b63f379b422e6
kushmirc <107213928+kushmirc@users.noreply.github.com>
2022-09-30 08:49:54 -0600

    Update onActionSavePart method.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -30,8 +30,6 @@ public class Main extends Application {
         Inventory.addPart(part1);
         Inventory.addPart(part2);
         Inventory.addPart(part3);
-        System.out.println(part1.getId() + part1.getName());
-
 
         Product product1 = new Product(1000, "The Addams Family", 3000, 6, 2, 10);
         Product product2 = new Product(1001, "Terminator 2", 2000, 3, 1, 5);
--- src/main/java/mirchandani/inventorysystem/AddPartController.java
+++ src/main/java/mirchandani/inventorysystem/AddPartController.java
@@ -11,6 +11,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import model.InHouse;
+import model.Inventory;
 
 import java.io.IOException;
 import java.net.URL;
@@ -63,9 +65,17 @@ public class AddPartController implements Initializable {
         String name = partNameTxt.getText();
         int stock = Integer.parseInt(partInvTxt.getText());
         double price = Double.parseDouble(partPriceCostTxt.getText());
+        int max = Integer.parseInt(partMaxTxt.getText());
+        int min = Integer.parseInt(partMinTxt.getText());
         boolean inHouse;
-        String machineID = partMachineIDTxt.getText();
+        int machineId = Integer.parseInt(partMachineIDTxt.getText());
 
+        if(partInHouseRBtn.isSelected())
+            inHouse = true;
+        else
+            inHouse = false;
+
+        Inventory.addPart(new InHouse(id, name, price, stock, min, max, machineId));
 
     }
 
